408b5fea911dd59eb8a0b6dd54d7ed7e42309ec0
mywaytomd <chornijj15@gmail.com>
2025-11-21 14:00:42 +0100

    fix

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const language = 'English';
+const language = 'ENG';
 
 const langLowerCase = language.toLowerCase();
 switch(langLowerCase) {
-    case 'english':
+    case 'eng':
         console.log('Hello!');
         break;
-    case 'spanish':
+    case 'esp':
         console.log('¡Hola!');
         break;
-    case 'french':
+    case 'fr':
         console.log('Bonjour!');
         break;
-    case 'german':
+    case 'de':
         console.log('Gutten Tag!');
+        break;
+    case 'ru':
+        console.log('Привет!');
         break;
     default:
         console.log('Language not supported.');
